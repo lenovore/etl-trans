755e31d637235917f475e8bc8d92df8d78dc6e8c
lenovore <18323089196@163.com>
2020-06-13 00:14:21 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,8 @@ sleep_sec=60
 log=/data/etl-trans/etl.log
 while :
 do
-    /usr/bin/pypy /data/etl-trans/etl-trans.py -r -m
+    cd /data/etl-trans && /usr/bin/pypy etl-trans.py -r -m
+    #/usr/bin/pypy /data/etl-trans/etl-trans.py -r -m
     echo $(date + "%F %T")" etl-trans停止，返回代码：$?" | tee -a $log
     echo "${sleep_sec}秒后再次启动" | tee -a $log
     sleep $sleep_sec
